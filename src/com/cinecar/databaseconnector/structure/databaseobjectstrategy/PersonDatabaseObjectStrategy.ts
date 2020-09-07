ce6df7450fbcdd53489348cc2f92ded3dbd6b16e
leoyn <leo@mixify.ga>
2020-09-07 23:09:24 +0200

    Implement PersonDatabaseObjectStrategy

--- src/com/cinecar/databaseconnector/structure/databaseobjectstrategy/PersonDatabaseObjectStrategy.ts
+++ src/com/cinecar/databaseconnector/structure/databaseobjectstrategy/PersonDatabaseObjectStrategy.ts
@@ -1,23 +1,76 @@
 import { DatabaseObjectStrategy } from "../DatabaseObjectStrategy";
+import { ConnectionSingleton } from "../ConnectionSingleton";
+import { Person } from "com.cinecar.objects";
 
 export class PersonDatabaseObjectStrategy implements DatabaseObjectStrategy {
     public create(object: object): Promise<object> {
-        throw new Error("Method not implemented.");
+        const person: Person = <Person>object;
+
+        return new Promise((resolve, reject) => {
+            ConnectionSingleton.getConnection().query("INSERT INTO person (firstname, lastname) VALUES(?, ?)", [person.getFirstname(), person.getLastname()], (err, res, fields) => {
+                if (err) reject(err);
+                else {
+                    person.setId(res.insertId);
+                    resolve(person);
+                }
+            });
+        });
     }
 
     public update(object: object): Promise<object> {
-        throw new Error("Method not implemented.");
+        const person: Person = <Person>object;
+        return new Promise((resolve, reject) => {
+            ConnectionSingleton.getConnection().query("UPDATE person SET firstname = ?, lastname = ?", [person.getFirstname(), person.getLastname()], (err, res, fields) => {
+                if (err) reject(err);
+                else resolve(person);
+            });
+        });
     }
 
     public delete(id: number): Promise<void> {
-        throw new Error("Method not implemented.");
+        return new Promise((resolve, reject) => {
+            ConnectionSingleton.getConnection().query("DELETE FROM person WHERE id = ?", [id], (err, res, fields) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
     }
 
     public get(id: number): Promise<object> {
-        throw new Error("Method not implemented.");
+        return new Promise((resolve, reject) => {
+            ConnectionSingleton.getConnection().query("SELECT id, firstname, lastname FROM person WHERE id = ?", [id], (err, res, fields) => {
+                if (err || res.length == 0) reject(err);
+                else {
+                    const person: Person = new Person();
+                    person.setId(id);
+                    person.setFirstname(res[0].firstname);
+                    person.setLastname(res[0].lastname);
+
+                    resolve(person);
+                }
+            });
+        });
     }
 
     public getAll(): Promise<object[]> {
-        throw new Error("Method not implemented.");
+        return new Promise((resolve, reject) => {
+            ConnectionSingleton.getConnection().query("SELECT id, firstname, lastname FROM person", (err, res, fields) => {
+                if (err || res.length == 0) reject(err);
+                else {
+                    const persons: Array<Person> = [];
+
+                    res.forEach((row) => {
+                        const person: Person = new Person();
+                        person.setId(row.id);
+                        person.setFirstname(row.firstname);
+                        person.setLastname(row.lastname);
+
+                        persons.push(person);
+                    });
+
+                    resolve(persons);
+                }
+            });
+        });
     }
 }
