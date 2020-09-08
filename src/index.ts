e4de84e3bf0d07fcf0d548e70c8efd52c0e4aa2e
leoyn <leo@mixify.ga>
2020-09-09 01:35:59 +0200

    Expose DatabaseConnector interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * from "./com/cinecar/databaseconnector/behaviour/DatabaseConnector";
 export * from "./com/cinecar/databaseconnector/behaviour/DatabaseConnectorImplementation";
 export * from "./com/cinecar/databaseconnector/structure/DatabaseObjectType";
