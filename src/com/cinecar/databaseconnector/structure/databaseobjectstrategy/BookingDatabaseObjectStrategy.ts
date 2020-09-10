231b79c4fa321a4d479a7020dee313141c447354
leoyn <leo@mixify.ga>
2020-09-10 02:12:26 +0200

    Fixed a SQL Bug when id was ambiguous

--- src/com/cinecar/databaseconnector/structure/databaseobjectstrategy/BookingDatabaseObjectStrategy.ts
+++ src/com/cinecar/databaseconnector/structure/databaseobjectstrategy/BookingDatabaseObjectStrategy.ts
@@ -7,20 +7,28 @@ export class BookingDatabaseObjectStrategy implements DatabaseObjectStrategy {
         const booking: Booking = <Booking>object;
 
         return new Promise((resolve, reject) => {
-            ConnectionSingleton.getConnection().query("INSERT INTO booking (personId, cancelled) VALUES(?, ?)", [booking.getPerson().getId(), booking.isCancelled()], async (err, res, fields) => {
-                if (err) reject(err);
-                else {
-                    if (booking.getTickets() != null) {
-                        await booking.getTickets().forEach(async (ticket) => {
-                            await ConnectionSingleton.getConnection().query("UPDATE ticket SET bookingId = ? WHERE id = ?", [booking.getId(), ticket.getId()], (err, res, fields) => {
-                                if (err) reject();
+            ConnectionSingleton.getConnection().query(
+                "INSERT INTO booking (personId, cancelled) VALUES(?, ?)",
+                [booking.getPerson().getId(), booking.isCancelled()],
+                async (err, res, fields) => {
+                    if (err) reject(err);
+                    else {
+                        if (booking.getTickets() != null) {
+                            await booking.getTickets().forEach(async (ticket) => {
+                                await ConnectionSingleton.getConnection().query(
+                                    "UPDATE ticket SET bookingId = ? WHERE id = ?",
+                                    [booking.getId(), ticket.getId()],
+                                    (err, res, fields) => {
+                                        if (err) reject();
+                                    }
+                                );
                             });
-                        });
-                    }
+                        }
 
-                    resolve(booking);
+                        resolve(booking);
+                    }
                 }
-            });
+            );
         });
     }
 
@@ -28,10 +36,14 @@ export class BookingDatabaseObjectStrategy implements DatabaseObjectStrategy {
         const booking: Booking = <Booking>object;
 
         return new Promise((resolve, reject) => {
-            ConnectionSingleton.getConnection().query("UPDATE booking SET personId = ?, cancelled = ? WHERE id = ?", [booking.getPerson().getId(), booking.isCancelled(), booking.getId()], (err, res, fields) => {
-                if (err) reject(err);
-                else resolve(booking);
-            });
+            ConnectionSingleton.getConnection().query(
+                "UPDATE booking SET personId = ?, cancelled = ? WHERE id = ?",
+                [booking.getPerson().getId(), booking.isCancelled(), booking.getId()],
+                (err, res, fields) => {
+                    if (err) reject(err);
+                    else resolve(booking);
+                }
+            );
         });
     }
 
@@ -41,118 +53,133 @@ export class BookingDatabaseObjectStrategy implements DatabaseObjectStrategy {
 
     public get(id: number): Promise<object> {
         return new Promise((resolve, reject) => {
-            ConnectionSingleton.getConnection().query("SELECT id, cancelled, personId, firstname, lastname FROM booking b INNER JOIN person p ON b.personId = p.id WHERE b.id = ?", [id], (err, res, fields) => {
-                if (err || res.length == 0) reject(err);
-                else {
-                    const booking: Booking = new Booking();
+            ConnectionSingleton.getConnection().query(
+                "SELECT b.id AS id, cancelled, personId, firstname, lastname FROM booking b INNER JOIN person p ON b.personId = p.id WHERE b.id = ?",
+                [id],
+                (err, res, fields) => {
+                    if (err || res.length == 0) reject(err);
+                    else {
+                        const booking: Booking = new Booking();
 
-                    booking.setCancelled(res[0].cancelled);
+                        booking.setCancelled(res[0].cancelled);
 
-                    const person = new Person();
-                    person.setFirstname(res[0].firstname);
-                    person.setLastname(res[0].lastname);
-                    booking.setPerson(person);
+                        const person = new Person();
+                        person.setFirstname(res[0].firstname);
+                        person.setLastname(res[0].lastname);
+                        booking.setPerson(person);
 
-                    ConnectionSingleton.getConnection().query("SELECT id, row, movieScreeningId, movieId, datetime, name, duration FROM ticket t INNER JOIN movieScreening ms ON t.movieScreeningId = ms.id INNER JOIN movie m ON ms.movieId = m.id WHERE t.bookingId = ?", [id], (err, res, fields) => {
-                        if (err) reject();
-                        else {
-                            const tickets: Array<Ticket> = [];
-                            res.forEach((row) => {
-                                const ticket: Ticket = new Ticket();
+                        ConnectionSingleton.getConnection().query(
+                            "SELECT t.id AS id, row, movieScreeningId, movieId, datetime, name, duration FROM ticket t INNER JOIN movieScreening ms ON t.movieScreeningId = ms.id INNER JOIN movie m ON ms.movieId = m.id WHERE t.bookingId = ?",
+                            [id],
+                            (err, res, fields) => {
+                                if (err) reject();
+                                else {
+                                    const tickets: Array<Ticket> = [];
+                                    res.forEach((row) => {
+                                        const ticket: Ticket = new Ticket();
 
-                                ticket.setId(row.id);
-                                ticket.setBooking(booking);
-                                ticket.setRow(row.row);
-                                const movieScreening: MovieScreening = new MovieScreening();
-                                const movie: Movie = new Movie();
+                                        ticket.setId(row.id);
+                                        ticket.setBooking(booking);
+                                        ticket.setRow(row.row);
+                                        const movieScreening: MovieScreening = new MovieScreening();
+                                        const movie: Movie = new Movie();
 
-                                movie.setId(row.movieId);
-                                movie.setDuration(row.duration);
-                                movie.setName(row.name);
+                                        movie.setId(row.movieId);
+                                        movie.setDuration(row.duration);
+                                        movie.setName(row.name);
 
-                                movieScreening.setMovie(movie);
-                                movieScreening.setDatetime(new Date(row.datetime));
-                                movieScreening.setId(row.movieScreeningId);
+                                        movieScreening.setMovie(movie);
+                                        movieScreening.setDatetime(new Date(row.datetime));
+                                        movieScreening.setId(row.movieScreeningId);
 
-                                ticket.setMovieScreening(movieScreening);
+                                        ticket.setMovieScreening(movieScreening);
 
-                                tickets.push(ticket);
-                            });
+                                        tickets.push(ticket);
+                                    });
 
-                            booking.setTickets(tickets);
+                                    booking.setTickets(tickets);
 
-                            resolve(booking);
-                        }
-                    });
+                                    resolve(booking);
+                                }
+                            }
+                        );
+                    }
                 }
-            });
+            );
         });
     }
 
     public getAll(): Promise<object[]> {
         return new Promise((resolve, reject) => {
-            ConnectionSingleton.getConnection().query("SELECT id, cancelled, personId, firstname, lastname FROM booking b INNER JOIN person p ON b.personId = p.id", (err, res, fields) => {
-                if (err) reject(err);
-                else {
-                    const promises: Array<Promise<Booking>> = [];
+            ConnectionSingleton.getConnection().query(
+                "SELECT b.id AS id, cancelled, personId, firstname, lastname FROM booking b INNER JOIN person p ON b.personId = p.id",
+                (err, res, fields) => {
+                    if (err) reject(err);
+                    else {
+                        const promises: Array<Promise<Booking>> = [];
 
-                    res.forEach((row) => {
-                        promises.push(
-                            new Promise((resolve, reject) => {
-                                const booking: Booking = new Booking();
+                        res.forEach((row) => {
+                            promises.push(
+                                new Promise((resolve, reject) => {
+                                    const booking: Booking = new Booking();
 
-                                booking.setCancelled(row.cancelled);
+                                    booking.setCancelled(row.cancelled);
 
-                                const person = new Person();
-                                person.setFirstname(row.firstname);
-                                person.setLastname(row.lastname);
-                                booking.setPerson(person);
+                                    const person = new Person();
+                                    person.setFirstname(row.firstname);
+                                    person.setLastname(row.lastname);
+                                    booking.setPerson(person);
 
-                                ConnectionSingleton.getConnection().query("SELECT id, row, movieScreeningId, movieId, datetime, name, duration FROM ticket t INNER JOIN movieScreening ms ON t.movieScreeningId = ms.id INNER JOIN movie m ON ms.movieId = m.id WHERE t.bookingId = ?", [booking.getId()], (err, res, fields) => {
-                                    if (err) reject();
-                                    else {
-                                        const tickets: Array<Ticket> = [];
+                                    ConnectionSingleton.getConnection().query(
+                                        "SELECT t.id AS id, row, movieScreeningId, movieId, datetime, name, duration FROM ticket t INNER JOIN movieScreening ms ON t.movieScreeningId = ms.id INNER JOIN movie m ON ms.movieId = m.id WHERE t.bookingId = ?",
+                                        [booking.getId()],
+                                        (err, res, fields) => {
+                                            if (err) reject();
+                                            else {
+                                                const tickets: Array<Ticket> = [];
 
-                                        res.forEach((row) => {
-                                            const ticket: Ticket = new Ticket();
+                                                res.forEach((row) => {
+                                                    const ticket: Ticket = new Ticket();
 
-                                            ticket.setId(row.id);
-                                            ticket.setBooking(booking);
-                                            ticket.setRow(row.row);
-                                            const movieScreening: MovieScreening = new MovieScreening();
-                                            const movie: Movie = new Movie();
+                                                    ticket.setId(row.id);
+                                                    ticket.setBooking(booking);
+                                                    ticket.setRow(row.row);
+                                                    const movieScreening: MovieScreening = new MovieScreening();
+                                                    const movie: Movie = new Movie();
 
-                                            movie.setId(row.movieId);
-                                            movie.setDuration(row.duration);
-                                            movie.setName(row.name);
+                                                    movie.setId(row.movieId);
+                                                    movie.setDuration(row.duration);
+                                                    movie.setName(row.name);
 
-                                            movieScreening.setMovie(movie);
-                                            movieScreening.setDatetime(new Date(row.datetime));
-                                            movieScreening.setId(row.movieScreeningId);
+                                                    movieScreening.setMovie(movie);
+                                                    movieScreening.setDatetime(new Date(row.datetime));
+                                                    movieScreening.setId(row.movieScreeningId);
 
-                                            ticket.setMovieScreening(movieScreening);
+                                                    ticket.setMovieScreening(movieScreening);
 
-                                            tickets.push(ticket);
-                                        });
+                                                    tickets.push(ticket);
+                                                });
 
-                                        booking.setTickets(tickets);
+                                                booking.setTickets(tickets);
 
-                                        resolve(booking);
-                                    }
-                                });
-                            })
-                        );
-                    });
-
-                    Promise.all(promises)
-                        .then((bookings: Array<Booking>) => {
-                            resolve(bookings);
-                        })
-                        .catch((err) => {
-                            reject(err);
+                                                resolve(booking);
+                                            }
+                                        }
+                                    );
+                                })
+                            );
                         });
+
+                        Promise.all(promises)
+                            .then((bookings: Array<Booking>) => {
+                                resolve(bookings);
+                            })
+                            .catch((err) => {
+                                reject(err);
+                            });
+                    }
                 }
-            });
+            );
         });
     }
 }
